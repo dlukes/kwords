8ebb2b004f0d3d07a39564fc0a98e0aacbbeea38
David Lukes <dafydd.lukes@gmail.com>
2015-06-14 22:05:57 +0200

    precompute relative freqs for ref corpus

--- precompute_corpus_frequencies.R
+++ precompute_corpus_frequencies.R
@@ -1,14 +1,18 @@
 library(dplyr)
 library(tidyr)
 
+N <- list(
+  araneum_polonicum_minus = 109947123
+)
+
 read_word_list <- function(file) {
   df <- tbl_df(read.csv2(file, as.is = TRUE, header = FALSE))
-  names(df) <- c("id", "key", "freq")
+  names(df) <- c("id", "key", "n")
   # df$key <- factor(tolower(df$key))
   df$key <- tolower(df$key)
-  df <- select(df, key, freq) %>%
+  df <- select(df, key, n) %>%
     group_by(key) %>%
-    summarise(freq = sum(freq))
+    summarise(rel_fq_refc = sum(n) * 1e6 / N$araneum_polonicum_minus)
 }
 
 w <- read_word_list("./data/araneum_polonicum_minus-word_list-freq>4.csv")
