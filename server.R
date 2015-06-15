2ecabf463f956ca2a18c379fe781856070bb8ac4
David Lukes <dafydd.lukes@gmail.com>
2015-06-15 16:58:34 +0200

    only start din computation if input textarea not empty

--- server.R
+++ server.R
@@ -5,11 +5,13 @@ source("kwords.R")
 shinyServer(function(input, output) {
 
   output$kwords_w <- renderDataTable({
-    withProgress(din_from_text(input$txt,
-                               input$hide_nonword,
-                               input$word_or_lemma),
-                 message = "Zpracovávám data...\n",
-                 value = 0)
+    if (nchar(input$txt) > 0) {
+      withProgress(din_from_text(input$txt,
+                                 input$hide_nonword,
+                                 input$word_or_lemma),
+                   message = "Zpracovávám data...\n",
+                   value = 0)
+    }
   })
 
 })
