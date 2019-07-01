e505eaa4bce870bc3e3438dcf93448472569377d
David Lukes <dafydd.lukes@gmail.com>
2019-07-01 17:03:19 +0200

    Some wip changes I don't have a clue about anymore

--- ui.R
+++ ui.R
@@ -1,34 +1,41 @@
 library(shiny)
 
-inputTextarea <- function(inputId, value = "") {
+inputTextarea <- function(inputId, value="") {
   tagList(
-    singleton(tags$head(tags$script(src = "textarea.js"))),
-    tags$textarea(id = inputId,
-             class = "inputtextarea",
+    singleton(tags$head(tags$script(src="textarea.js"))),
+    tags$textarea(id=inputId,
+             class="inputtextarea",
              as.character(value)))
 }
 
-sidebarPanelWithLogo <- function (..., width = 4)  {
-  div(class = paste0("col-sm-", width),
-      tags$form(class = "well",  ...),
-      img(src = "https://trnka.korpus.cz/index-doc/logo/CNC-sirka-01-col-RGB.png",
-          width = "66%"))
+sidebarPanelWithLogo <- function (..., width=4)  {
+  div(class=paste0("col-sm-", width),
+      tags$form(class="well",  ...),
+      tags$br(),
+      tags$br(),
+      img(src="https://trnka.korpus.cz/index-doc/logo/CNC-sirka-01-col-RGB.png",
+          width="66%"),
+      tags$br(),
+      tags$br(),
+      tags$br(),
+      p("Lemmatization (as part of morpho-syntactic tagging) by",
+        a(href="http://nlp.pwr.wroc.pl/takipi/", target="_blank", "TaKIPI.")))
 }
 
 shinyUI(fluidPage(
   tags$head(
-    tags$link(rel = "shortcut icon", href = "favicon.ico"),
-    tags$link(rel = "stylesheet", type = "text/css", href = "kwords.css")),
+    tags$link(rel="shortcut icon", href="favicon.ico"),
+    tags$link(rel="stylesheet", type="text/css", href="kwords.css")),
 
   titlePanel("KWords.pl"),
 
   sidebarPanelWithLogo(radioButtons("word_or_lemma",
                                     "Estimate keywords based on:",
-                                    c("lemma frequencies (first run slower because of lemmatization)" = "l",
-                                      "word form frequencies" = "w")),
+                                    c("lemma frequencies (first run slower because of lemmatization)"="l",
+                                      "word form frequencies"="w")),
                        checkboxInput("hide_nonword",
                                      "Hide numbers and punctuation",
-                                     value = TRUE)),
+                                     value=TRUE)),
 
   mainPanel(
     tabsetPanel(
@@ -47,12 +54,12 @@ shinyUI(fluidPage(
       tabPanel("Help",
                tags$br(),
                p("See help for the original KWords app on the",
-                 a(href = "http://wiki.korpus.cz/doku.php/manualy:kwords",
-                   target = "_blank",
+                 a(href="http://wiki.korpus.cz/doku.php/manualy:kwords",
+                   target="_blank",
                    "CNC wiki."),
                  "The reference frequency figures against which the keyness of words in the input text is estimated are taken from the",
-                 a(href = "http://sketch.juls.savba.sk/aranea_about/index.html",
-                   target = "_blank",
+                 a(href="http://sketch.juls.savba.sk/aranea_about/index.html",
+                   target="_blank",
                    "Araneum Polonicum Minus"),
                  "corpus, compiled by Vlado Benko (JÚĽŠ SAV)."))))
 
